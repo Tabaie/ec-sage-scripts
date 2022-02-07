18e45e431a2670e3e67a8dc3e4a1577fa63c5a38
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2022-02-07 13:19:53 -0500

    args

--- bls12-381-g2-sqrtratio-consts.sage.py
+++ bls12-381-g2-sqrtratio-consts.sage.py
@@ -1,4 +1,12 @@
 from sage.all import *
+import argparse, sys
+
+parser = argparse.ArgumentParser('Compute stuff for hash to curve')
+parser.add_argument('--stop_at_c6', action='store_true')
+parser.add_argument('--stop_at_c7', action='store_true')
+parser.add_argument('--c6', type=str, default='not-provided')
+
+args = parser.parse_args()
 
 rz = IntegerRing()
 
@@ -19,10 +27,10 @@ while qP % 2 == 0:
     qP /= 2
 print('c1 =', c1)
 
-c2 = (q - 1) / (2^c1)        # Integer arithmetic
+c2 = (q - 1) / (2**c1)        # Integer arithmetic
 print('c2 =', c2)
 
-c3 = (c2 - 1) / 2            # Integer arithmetic
+c3 = (c2 - 1) / 2        # Integer arithmetic
 print('c3 =', c3)
 
 c4 = 2**c1 - 1                # Integer arithmetic
@@ -31,8 +39,18 @@ print('c4 =', c4)
 c5 = 2**(c1 - 1)              # Integer arithmetic
 print('c5 =', c5)
 
-c6 = Z**rz(c2)
+if args.stop_at_c6:
+    sys.exit(0)
+
+if args.c6 == 'not-provided':
+    c6 = Z**rz(c2)
+else:
+    c6 = rz(args.c6)
+
 print('c6 =', c6)
+
+if args.stop_at_c7:
+    sys.exit(0)
 
 c7 = Z**rz((c2 + 1) / 2)
 print('c7 =', c7)
